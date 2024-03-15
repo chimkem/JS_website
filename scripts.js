a30cc03001f1b4adc5853c9db11c4adde4821cca
chimkem <126956034+chimkem@users.noreply.github.com>
2024-03-15 06:03:38 +0200

    Här är Gilbert

--- scripts.js
+++ scripts.js
@@ -1,9 +1,23 @@
 const listapohja = document.getElementById('tehtavapohja');  // Pohja
 const tehtavaInput = document.getElementById('tehtava'); // Tekstikenttä
 
-// V To-Do:n scripti: V
+//---------------------
+// Selaimen välimuisti
+//---------------------
 
-// Lisää tehtävä
+// Haetaan välimuisti
+function tehtavamuisti(){
+    listapohja.innerHTML = localStorage.getItem("ToDo");
+}
+// Tallennus välimuistiin
+function tallenna(){
+    localStorage.setItem("ToDo", tehtavapohja.innerHTML);
+}
+
+//----------------------------
+// Tehtävien lisäys ja poisto
+//----------------------------
+
 function lisaa() {
     // Jos tehtäväkenttä ei ole tyhjä, lisätään tehtävä listaan.
     if (tehtavaInput.value !== '') {
@@ -11,15 +25,23 @@ function lisaa() {
         li.innerHTML = tehtavaInput.value;
 
         let tehtavapohja = document.getElementById('tehtavapohja');
-        tehtavapohja.appendChild(li);
-
-    // lisätään perään ikoni poistamiselle bootstrapilla
+        
+        // Halusin lisätä nämä listaan siten, että uusin on korkeimpana.
+        if (tehtavapohja.firstChild) {
+            tehtavapohja.insertBefore(li, tehtavapohja.firstChild);
+        }
+        else {
+            tehtavapohja.appendChild(li);
+        }
+            
+        // lisätään perään ikoni poistamiselle
         let del = document.createElement("span");
-        del.innerHTML = "\uF6B6";
+        del.innerHTML = "❌";
         li.appendChild(del);
 
         //Tyhjennetään tekstikenttä lähetyksen jälkeen ja logataan onnistuminen
         tehtavaInput.value = '';
+        tallenna();
         console.log("Tehtävä lisätty listaan");
     } 
     // ... Muuten soitetaan äänimerkki ja lyödään punainen reuna tehtävälistan ympärille kahdeksi sekunniksi.
@@ -36,30 +58,43 @@ function lisaa() {
         console.log("Tyhjä kenttä!");
     }
 }
-// Suorita tehtävä
-function suorita() {
 
-}
-// Poista tehtävä
-function poista() {
 
-}
-// Tallenna tiedot sivun välimuistiin
-function tallenna() {
+//-------------------------
+// Tehtävien merkitseminen
+//-------------------------
 
-}
+//Jos klikataan tehtävää, se merkitään tehdyksi
+listapohja.addEventListener('click', function(teht){
+    if (teht.target.tagName === "LI") {
+        teht.target.classList.toggle("valmis");
+
+        tallenna();
+        console.log("Tehtävän tila on muuttunut");
+    }
+// Jos klikataan ruksia, se poistetaan välimuistista ja soitetaan äänimerkki
+    else if (teht.target.tagName === "SPAN"){
+        teht.target.parentElement.remove();
+        merkkiaani('gallery/audio/Wood.mp3')
+        tallenna();
+    }
+}, false);
+
 // Extra: Muokkaa tehtävää
 
 
 
-// V Extrajuttuja V
+//--------
+// Extra
+//--------
+
 // Ilmoitetaan ettei kirjautuminen onnistu.
 function login() {
     alert("Jotain meni vikaan, yritä myöhemmin uudelleen!");
 }
 
 // Hampparimenu! :D
-// Avautuu vain puhelimella klikkaamalla logoa, muuten heittää erroria konsoliin
+// Avautuu vain puhelimella klikkaamalla logoa, muuten heittää vain tekstiä konsoliin
 function openMenu() {
     var navbar = document.getElementById('navbar');
 
@@ -68,13 +103,16 @@ function openMenu() {
         merkkiaani('/gallery/audio/Cartoon.mp3');
     }
     else {
-        console.log(":)");
+        console.log("Här är Gilbert");
     }
 }
 
-// Lisätään merkkiäänimahdollisuus, koska miksi ei...
+// Lisätään merkkiäänimahdollisuuksia, koska miksi ei...
 function merkkiaani(polku) {
     var merkki = new Audio(polku)
     merkki.loop = false;
     merkki.play();
 }
+
+// Täällä lopussa ajetaan komento lukea välimuistista tehtävät
+tehtavamuisti();
