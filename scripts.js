554574e05337ac3632f7ba769458c9a1d4c8d403
chimkem <126956034+chimkem@users.noreply.github.com>
2024-03-15 05:03:43 +0200

    Hoplaa

--- scripts.js
+++ scripts.js
@@ -1,33 +1,39 @@
-const listapohja = document.getElementById('tehtavapohja');
-const tehtavaInput = document.getElementById('tehtava');
+const listapohja = document.getElementById('tehtavapohja');  // Pohja
+const tehtavaInput = document.getElementById('tehtava'); // Tekstikenttä
 
-// To-Do
+// V To-Do:n scripti: V
 
 // Lisää tehtävä
 function lisaa() {
-    // Jos tehtäväkenttä on ei ole tyhjä, lisätään tehtävä listaan.
-    if(tehtavaInput.value !== '') {
+    // Jos tehtäväkenttä ei ole tyhjä, lisätään tehtävä listaan.
+    if (tehtavaInput.value !== '') {
         let li = document.createElement("li");
         li.innerHTML = tehtavaInput.value;
-        listapohja.appendChild(li);
 
-        
-        let span = document.createElement("span");
-        span.innerHTML = "\u00d7";
-        li.appendChild(span);
+        let tehtavapohja = document.getElementById('tehtavapohja');
+        tehtavapohja.appendChild(li);
 
-        //Tyhjennetään tekstikenttä lähetyksen jälkeen
+    // lisätään perään ikoni poistamiselle bootstrapilla
+        let del = document.createElement("span");
+        del.innerHTML = "\uF6B6";
+        li.appendChild(del);
+
+        //Tyhjennetään tekstikenttä lähetyksen jälkeen ja logataan onnistuminen
         tehtavaInput.value = '';
+        console.log("Tehtävä lisätty listaan");
     } 
     // ... Muuten soitetaan äänimerkki ja lyödään punainen reuna tehtävälistan ympärille kahdeksi sekunniksi.
-    else{
-        playSound2();
+    else {
+        merkkiaani('/gallery/audio/Metallic.mp3');
         tehtava.style.border = "1px solid #ec7166";
         tehtava.style.borderRadius = "10px";
 
         setTimeout(function() {
             tehtava.style.border = "";
         }, 2000);
+
+    // Tästäkin logit konsoliin
+        console.log("Tyhjä kenttä!");
     }
 }
 // Suorita tehtävä
@@ -44,26 +50,31 @@ function tallenna() {
 }
 // Extra: Muokkaa tehtävää
 
-// Kirjautumisnappi
+
+
+// V Extrajuttuja V
+// Ilmoitetaan ettei kirjautuminen onnistu.
 function login() {
     alert("Jotain meni vikaan, yritä myöhemmin uudelleen!");
 }
 
-// Merkkiäänet, koska miksi ei
-function playSound() {
-    var sound = new Audio('/gallery/audio/Cartoon.mp3')
-    sound.loop = false;
-    sound.play();
-
-    
-}
-function playSound2() {
-    var metalli = new Audio('/gallery/audio/Metallic Clank.mp3')
-    metalli.loop = false;
-    metalli.play();
-}
-
+// Hampparimenu! :D
+// Avautuu vain puhelimella klikkaamalla logoa, muuten heittää erroria konsoliin
 function openMenu() {
     var navbar = document.getElementById('navbar');
-    navbar.classList.toggle('show');
+
+    if (window.innerWidth <= 768) {
+        navbar.classList.toggle('show');
+        merkkiaani('/gallery/audio/Cartoon.mp3');
+    }
+    else {
+        console.log(":)");
+    }
+}
+
+// Lisätään merkkiäänimahdollisuus, koska miksi ei...
+function merkkiaani(polku) {
+    var merkki = new Audio(polku)
+    merkki.loop = false;
+    merkki.play();
 }
